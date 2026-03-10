6348198545ef1f4b2aecc122c8755a60d058c040
damiancastelao <damian@danielcastelao.org>
2026-03-10 10:05:59 +0100

    Creación de naves y colocación en el tablero

--- nave.py
+++ nave.py
@@ -1,12 +1,13 @@
 # Clase que representa una nave en el juego
 class Nave:
-    def __init__(self, nombre, tamano):
+    def __init__(self, nombre, tipo, vida):
         """
         Constructor de la clase Nave.
         
         Args:
-            nombre (str): Nombre del barco (Submarino, Buque, Portaaviones)
-            tamano (int): Tamaño de la nave (número de casillas que ocupa)
+            nombre (str): Nombre de la nave (string)
+            tipo (str): Nombre del barco (submarino, fragata, portaaviones)
+            vida (int): Tamaño de la nave (número de casillas que ocupa)
         """
         pass
 
